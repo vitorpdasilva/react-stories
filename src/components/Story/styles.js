f6873ad13541c3aaf2303933d0953ff54b5ee7aa
Vitor Boccio <vitorboccio@gmail.com>
2020-07-07 17:26:33 +1000

    story style

--- src/components/Story/styles.js
+++ src/components/Story/styles.js
@@ -15,9 +15,21 @@ const StyledAvatar = styled.div`
     box-sizing: border-box;
     cursor: pointer;
     img {
-      width: 100%;
-      height: 100%;
       border-radius: 50%;
+      width: 100%;
+      height: auto;
+      display: block;
+      position: relative;
+      min-height: 55px;
+      margin-bottom: 5px;
+      &:before {
+        content: '';
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        background-color: rgb(230, 230, 230);
+      }
     }
     p {
       font-size: 12px;
